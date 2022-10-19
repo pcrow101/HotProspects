6cdd534dfe7bce35959a6e70944115f16b41e5ca
pcrow101 <34235799+pcrow101@users.noreply.github.com>
2022-10-19 13:28:17 +0100

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by paul crow on 19/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
